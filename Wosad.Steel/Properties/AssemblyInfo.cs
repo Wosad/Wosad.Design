3071e99dadfa8f6bc10769f5b039a6851add3f87
Konstantin <wosad@outlook.com>
2015-12-22 02:56:50 -0500

    Updates of Nuget packages

--- Wosad.Steel/Properties/AssemblyInfo.cs
+++ Wosad.Steel/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Wosad.Steel")]
-[assembly: AssemblyCopyright("Copyright Wosad Inc � 2015")]
+[assembly: AssemblyCopyright("Copyright Wosad Inc © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.2")]
-[assembly: AssemblyFileVersion("0.0.1.2")]
+[assembly: AssemblyVersion("0.0.1.3")]
+[assembly: AssemblyFileVersion("0.0.1.3")]
